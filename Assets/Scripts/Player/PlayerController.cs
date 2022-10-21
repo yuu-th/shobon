7c5ecbd26068b59bc99b9ad685b15f6a82dd9ddc
maguro256 <1030seima1030@gmail.com>
2022-10-21 18:13:47 +0900

    昇天回避

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -108,7 +108,6 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         jumpAfterFrame++;
-        //rigid2D.velocity = new Vector2(rigid2D.velocity.x + 0.1f*Input.GetAxis("Horizontal"), rigid2D.velocity.y);
 
         float horizontal = Input.GetAxis("Horizontal");
 
@@ -269,6 +268,7 @@ public class PlayerController : MonoBehaviour
         BoxCollider2D head_collider,foot_collider,player_collider;
         SpriteRenderer render;
 
+        dead = true;
         head = this.transform.GetChild(0).gameObject;
         foot = this.transform.GetChild(1).gameObject;
         head_collider = head.GetComponent<BoxCollider2D>();
