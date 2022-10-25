4a5d3dcc01c06865c30fc3de556d125bed82adf5
nonor102 <105913423+nonor102@users.noreply.github.com>
2022-10-25 17:59:59 +0900

    to 増永

---
+++ Assets/everyone/masunaga/scripts/generator_controller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator_controller : MonoBehaviour
+{
+    
+    public GameObject prefab;
+    public float delay;
+    float first_time,end_time;
+    void Start()
+    {
+        first_time = Time.time;
+        end_time = Time.time + delay;
+    }
+
+    void Update()
+    {
+        Debug.Log(end_time - first_time);
+        if(end_time - first_time >= delay) 
+        {
+            Vector3 pos = new Vector3(this.transform.position.x,this.transform.position.y,0);
+            Instantiate(prefab,pos,Quaternion.identity);
+            first_time = Time.time;
+            end_time = Time.time;
+        }
+        end_time += 1;
+    }
+}
