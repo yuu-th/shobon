a584e341e51cabc58e572d5f4bb3ad25f437a32a
yuu_th_-ubuntu <yutakato333@gmail.com>
2022-10-27 21:31:51 +0900

    really complete UI SELECTOR

--- Assets/UI/STAGE_SELECTOR_CONTROLLER.cs
+++ Assets/UI/STAGE_SELECTOR_CONTROLLER.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System.IO;
+using UnityEditor;
 
 public class STAGE_SELECTOR_CONTROLLER : MonoBehaviour
 {
+    public List<List<int>> Data;
     public GameObject back_stage;
     private string scene_path = "./Assets/Scenes";
+    private string[] level_path_array = {"STAGE_EASY","STAGE_NORMAL","STAGE_HARD","STAGE_EXPERT","STAGE_MASTER"};
     private string[] level_array = {"EASY","NORMAL","HARD","EXPERT","MASTER"};
     public UIDocument uIDocument = null;
     private VisualElement root;
@@ -17,6 +21,8 @@ public class STAGE_SELECTOR_CONTROLLER : MonoBehaviour
     private List<VisualElement> level_wrapper_array = new List<VisualElement>();
     void Start()
     {
+        Debug.Log(Application.persistentDataPath );
+        
         this.back_stage.SetActive(false);
         PlayerPrefs.SetInt("LIFE", 5);
         PlayerPrefs.Save();
@@ -35,11 +41,18 @@ public class STAGE_SELECTOR_CONTROLLER : MonoBehaviour
         int i =0 ;
         root.Query<VisualElement>("level_wrapper").ForEach((VisualElement ele) => {
             this.level_wrapper_array.Add(ele);
-            string[] stage_array = System.IO.Directory.GetFiles(this.scene_path+"/"+ this.level_array[i] ,"*.unity",System.IO.SearchOption.AllDirectories);
+            //string[] stage_array = System.IO.Directory.GetFiles(this.scene_path+"/"+ this.level_array[i] ,"*.unity",System.IO.SearchOption.AllDirectories);
+            //string[] stage_array = UnityEditor.EditorBuildSettings.scenes;
             //Debug.Log(stage_array[0]);
-            foreach(string stage in stage_array){
+            //foreach(var stage in SceneManager.GetActiveScene()){
+            for(int j = 0; j < SceneManager.sceneCountInBuildSettings; j++){
                 //stageのファイル構造の最後のファイル名をstage_nameに代入
-                string stage_name = stage.Split('\\','/','.')[stage.Split('\\','/','.').Length-2];
+                var stage =  SceneUtility.GetScenePathByBuildIndex(j);
+                if (stage.Split('/','\\')[stage.Split('/','\\').Length-2] != this.level_path_array[i]){
+                    continue;
+                }
+                string stage_name =stage.Split('/','\\','.')[stage.Split('/','\\','.').Length-2];
+                Debug.Log(stage_name);
                 Button button = new Button();
                 button.name = this.level_array[i]+"_button";
                 button.text = stage_name;
@@ -50,7 +63,6 @@ public class STAGE_SELECTOR_CONTROLLER : MonoBehaviour
                     SceneManager.LoadScene(stage_name);
                 };
                 ele.Add(button);
-                Debug.Log(stage_name);
             }
             i++;
         });
